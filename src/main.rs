5dd7679ec175abbf96c0486919ccf7ac491b5378
David Deprost <dadeprost@gmail.com>
2022-12-04 19:22:11 +0100

    Add first markup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 use dioxus::prelude::*;
 
 fn main() {
-    dioxus::web::launch(app);
+  dioxus::web::launch(app);
 }
 
 fn app(cx: Scope) -> Element {
-    cx.render(rsx!{
-        div { "hello, wasm!" }
-    })
+  cx.render(rsx!{
+    style { [include_str!("../src/style.css")] }
+    div {
+      h1 { "Subtitle Modifier"}
+      ul {
+        li {
+          h3 { "Select a subtitle file:" }
+          label { r#for: "subfile", "Subtitle file: "}
+          input { r#type: "file" , id: "subfile"}
+        }
+      }
+      
+    }
+    
+    
+  })
 }
